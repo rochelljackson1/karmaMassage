1964d37c296ca9f37a17fea9af71a6083d680983
rochelljackson1 <rochelljackson1@gmail.com>
2020-10-31 16:57:00 -0500

    Rachel 10/31/2020 @4:57 PM
    DataHandler has customerTable

---
+++ src/main/java/com/uh/rachel/util/DataHandler.java
@@ -0,0 +1,31 @@
+package com.uh.rachel.util;
+
+import com.uh.rachel.util.tableClasses.customerTable;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+public class DataHandler {
+    public static Vector<customerTable> getCustomers() {
+        Vector<customerTable> v = new Vector<>();
+        try {
+            Connection connection = ConnectionProvider.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM customerTable");
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                v.add(new customerTable(result.getInt("customerNumber"),
+                        result.getInt("storeCredit"), result.getString("firstName"),
+                        result.getString("lastName"), result.getString("gender"),
+                        result.getString("phone"), result.getString("email"),
+                        result.getString("birthday"), result.getString("address")));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return v;
+    }
+}
