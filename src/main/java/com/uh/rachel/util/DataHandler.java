ea02489de9d3f753bb2f94d0b56af2b788da4041
rochelljackson1 <rochelljackson1@gmail.com>
2020-11-01 12:32:00 -0600

    Rachel 101/01/2020 @12:32 PM
    Now has customerTable Vector and doctorTable Vector

--- src/main/java/com/uh/rachel/util/DataHandler.java
+++ src/main/java/com/uh/rachel/util/DataHandler.java
@@ -1,6 +1,7 @@
 package com.uh.rachel.util;
 
 import com.uh.rachel.util.tableClasses.customerTable;
+import com.uh.rachel.util.tableClasses.doctorTable;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -21,6 +22,25 @@ public class DataHandler {
                         result.getString("lastName"), result.getString("gender"),
                         result.getString("phone"), result.getString("email"),
                         result.getString("birthday"), result.getString("address")));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return v;
+    }
+
+    public static Vector<doctorTable> getDoctors() {
+        Vector<doctorTable> v = new Vector<>();
+        try {
+            Connection connection = ConnectionProvider.getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM doctorTable");
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                v.add(new doctorTable(result.getInt("customerNumber"),
+                        result.getInt("doctorNuber"), result.getString("physicianFirstname"),
+                        result.getString("physicianLastname"), result.getString("physicianPhone")));
             }
 
         } catch (Exception e) {
