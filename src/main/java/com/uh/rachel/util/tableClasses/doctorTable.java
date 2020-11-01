bab9c43b6e95de255546d236e16e95119922ff53
rochelljackson1 <rochelljackson1@gmail.com>
2020-11-01 12:25:56 -0600

    Rachel 11/01/2020 @12:26 PM
    Creating doctorTable

---
+++ src/main/java/com/uh/rachel/util/tableClasses/doctorTable.java
@@ -0,0 +1,61 @@
+package com.uh.rachel.util.tableClasses;
+
+public class doctorTable {
+    int customerNumber;
+    int doctorNuber;
+    String physicianFirstname;
+    String PhysicianLastname;
+    String physicianPhone;
+
+    public doctorTable() {
+
+    }
+
+    public doctorTable(int customerNumber, int doctorNuber, String physicianFirstname, String physicianLastname, String physicianPhone) {
+        this.customerNumber = customerNumber;
+        this.doctorNuber = doctorNuber;
+        this.physicianFirstname = physicianFirstname;
+        PhysicianLastname = physicianLastname;
+        this.physicianPhone = physicianPhone;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public int getDoctorNuber() {
+        return doctorNuber;
+    }
+
+    public void setDoctorNuber(int doctorNuber) {
+        this.doctorNuber = doctorNuber;
+    }
+
+    public String getPhysicianFirstname() {
+        return physicianFirstname;
+    }
+
+    public void setPhysicianFirstname(String physicianFirstname) {
+        this.physicianFirstname = physicianFirstname;
+    }
+
+    public String getPhysicianLastname() {
+        return PhysicianLastname;
+    }
+
+    public void setPhysicianLastname(String physicianLastname) {
+        PhysicianLastname = physicianLastname;
+    }
+
+    public String getPhysicianPhone() {
+        return physicianPhone;
+    }
+
+    public void setPhysicianPhone(String physicianPhone) {
+        this.physicianPhone = physicianPhone;
+    }
+}
