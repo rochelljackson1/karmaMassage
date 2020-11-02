268689f7617669856d542737dee4c590eafe5884
meido1984 <Wateen@2016>
2020-11-02 01:36:02 -0600

    Makki 11/02 @ 1:35AM adding Address.class, and .jsp as well revenue.jsp deleted old Address.jsp since it was under wrong section.

---
+++ src/main/java/com/uh/rachel/util/tableClasses/Address.java
@@ -0,0 +1,75 @@
+package com.uh.rachel.util.tableClasses;
+
+public class Address {
+    int customerNumber;
+    String street;
+    String cityName;
+    String stateName;
+    String zipCode;
+    String countryName;
+
+
+    public Address() {
+
+    }
+
+    public Address(int customerNumber, String street, String cityName, String stateName, String zipCode, String countryName) {
+        this.customerNumber = customerNumber;
+        this.street = street;
+        this.cityName = cityName;
+        this.stateName = stateName;
+        this.zipCode = zipCode;
+        this.countryName = countryName;
+
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+
+}
