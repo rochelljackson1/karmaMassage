b7883440d5f0d08e7d5659eef60974aeb5a2dafc
rochelljackson1 <rochelljackson1@gmail.com>
2020-11-04 15:21:50 -0600

    Rachel 11/04/2020 @3:22 PM
    Creating tableClasses.

---
+++ src/main/java/com/uh/rachel/util/tableClasses/existingConditionsSTJDTable.java
@@ -0,0 +1,260 @@
+package com.uh.rachel.util.tableClasses;
+
+public class existingConditionsSTJDTable {
+    int customerNumber;
+    boolean upperBackRight;
+    boolean upperBackLeft;
+    boolean shouldersRight;
+    boolean shouldersLeft;
+    boolean neckRight;
+    boolean neckLeft;
+    boolean midBackRight;
+    boolean midBackLeft;
+    boolean lowerBackRight;
+    boolean lowerBackLeft;
+    boolean legsRight;
+    boolean legsLeft;
+    boolean kneesRight;
+    boolean kneesLeft;
+    boolean hipsRight;
+    boolean hipsLeft;
+    boolean handsRight;
+    boolean handsLeft;
+    boolean feetRight;
+    boolean feetLeft;
+    boolean armsRight;
+    boolean armsLeft;
+    boolean anklesRight;
+    boolean anklesLeft;
+
+    public existingConditionsSTJDTable() {
+    }
+
+    public existingConditionsSTJDTable(int customerNumber, boolean upperBackRight, boolean upperBackLeft, boolean shouldersRight, boolean shouldersLeft, boolean neckRight, boolean neckLeft, boolean midBackRight, boolean midBackLeft, boolean lowerBackRight, boolean lowerBackLeft, boolean legsRight, boolean legsLeft, boolean kneesRight, boolean kneesLeft, boolean hipsRight, boolean hipsLeft, boolean handsRight, boolean handsLeft, boolean feetRight, boolean feetLeft, boolean armsRight, boolean armsLeft, boolean anklesRight, boolean anklesLeft) {
+        this.customerNumber = customerNumber;
+        this.upperBackRight = upperBackRight;
+        this.upperBackLeft = upperBackLeft;
+        this.shouldersRight = shouldersRight;
+        this.shouldersLeft = shouldersLeft;
+        this.neckRight = neckRight;
+        this.neckLeft = neckLeft;
+        this.midBackRight = midBackRight;
+        this.midBackLeft = midBackLeft;
+        this.lowerBackRight = lowerBackRight;
+        this.lowerBackLeft = lowerBackLeft;
+        this.legsRight = legsRight;
+        this.legsLeft = legsLeft;
+        this.kneesRight = kneesRight;
+        this.kneesLeft = kneesLeft;
+        this.hipsRight = hipsRight;
+        this.hipsLeft = hipsLeft;
+        this.handsRight = handsRight;
+        this.handsLeft = handsLeft;
+        this.feetRight = feetRight;
+        this.feetLeft = feetLeft;
+        this.armsRight = armsRight;
+        this.armsLeft = armsLeft;
+        this.anklesRight = anklesRight;
+        this.anklesLeft = anklesLeft;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public boolean isUpperBackRight() {
+        return upperBackRight;
+    }
+
+    public void setUpperBackRight(boolean upperBackRight) {
+        this.upperBackRight = upperBackRight;
+    }
+
+    public boolean isUpperBackLeft() {
+        return upperBackLeft;
+    }
+
+    public void setUpperBackLeft(boolean upperBackLeft) {
+        this.upperBackLeft = upperBackLeft;
+    }
+
+    public boolean isShouldersRight() {
+        return shouldersRight;
+    }
+
+    public void setShouldersRight(boolean shouldersRight) {
+        this.shouldersRight = shouldersRight;
+    }
+
+    public boolean isShouldersLeft() {
+        return shouldersLeft;
+    }
+
+    public void setShouldersLeft(boolean shouldersLeft) {
+        this.shouldersLeft = shouldersLeft;
+    }
+
+    public boolean isNeckRight() {
+        return neckRight;
+    }
+
+    public void setNeckRight(boolean neckRight) {
+        this.neckRight = neckRight;
+    }
+
+    public boolean isNeckLeft() {
+        return neckLeft;
+    }
+
+    public void setNeckLeft(boolean neckLeft) {
+        this.neckLeft = neckLeft;
+    }
+
+    public boolean isMidBackRight() {
+        return midBackRight;
+    }
+
+    public void setMidBackRight(boolean midBackRight) {
+        this.midBackRight = midBackRight;
+    }
+
+    public boolean isMidBackLeft() {
+        return midBackLeft;
+    }
+
+    public void setMidBackLeft(boolean midBackLeft) {
+        this.midBackLeft = midBackLeft;
+    }
+
+    public boolean isLowerBackRight() {
+        return lowerBackRight;
+    }
+
+    public void setLowerBackRight(boolean lowerBackRight) {
+        this.lowerBackRight = lowerBackRight;
+    }
+
+    public boolean isLowerBackLeft() {
+        return lowerBackLeft;
+    }
+
+    public void setLowerBackLeft(boolean lowerBackLeft) {
+        this.lowerBackLeft = lowerBackLeft;
+    }
+
+    public boolean isLegsRight() {
+        return legsRight;
+    }
+
+    public void setLegsRight(boolean legsRight) {
+        this.legsRight = legsRight;
+    }
+
+    public boolean isLegsLeft() {
+        return legsLeft;
+    }
+
+    public void setLegsLeft(boolean legsLeft) {
+        this.legsLeft = legsLeft;
+    }
+
+    public boolean isKneesRight() {
+        return kneesRight;
+    }
+
+    public void setKneesRight(boolean kneesRight) {
+        this.kneesRight = kneesRight;
+    }
+
+    public boolean isKneesLeft() {
+        return kneesLeft;
+    }
+
+    public void setKneesLeft(boolean kneesLeft) {
+        this.kneesLeft = kneesLeft;
+    }
+
+    public boolean isHipsRight() {
+        return hipsRight;
+    }
+
+    public void setHipsRight(boolean hipsRight) {
+        this.hipsRight = hipsRight;
+    }
+
+    public boolean isHipsLeft() {
+        return hipsLeft;
+    }
+
+    public void setHipsLeft(boolean hipsLeft) {
+        this.hipsLeft = hipsLeft;
+    }
+
+    public boolean isHandsRight() {
+        return handsRight;
+    }
+
+    public void setHandsRight(boolean handsRight) {
+        this.handsRight = handsRight;
+    }
+
+    public boolean isHandsLeft() {
+        return handsLeft;
+    }
+
+    public void setHandsLeft(boolean handsLeft) {
+        this.handsLeft = handsLeft;
+    }
+
+    public boolean isFeetRight() {
+        return feetRight;
+    }
+
+    public void setFeetRight(boolean feetRight) {
+        this.feetRight = feetRight;
+    }
+
+    public boolean isFeetLeft() {
+        return feetLeft;
+    }
+
+    public void setFeetLeft(boolean feetLeft) {
+        this.feetLeft = feetLeft;
+    }
+
+    public boolean isArmsRight() {
+        return armsRight;
+    }
+
+    public void setArmsRight(boolean armsRight) {
+        this.armsRight = armsRight;
+    }
+
+    public boolean isArmsLeft() {
+        return armsLeft;
+    }
+
+    public void setArmsLeft(boolean armsLeft) {
+        this.armsLeft = armsLeft;
+    }
+
+    public boolean isAnklesRight() {
+        return anklesRight;
+    }
+
+    public void setAnklesRight(boolean anklesRight) {
+        this.anklesRight = anklesRight;
+    }
+
+    public boolean isAnklesLeft() {
+        return anklesLeft;
+    }
+
+    public void setAnklesLeft(boolean anklesLeft) {
+        this.anklesLeft = anklesLeft;
+    }
+}
---
+++ src/main/java/com/uh/rachel/util/tableClasses/reasonForMassageTable.java
@@ -0,0 +1,60 @@
+package com.uh.rachel.util.tableClasses;
+
+public class reasonForMassageTable {
+    int customerNumber;
+    String pastTreatment;
+    String treatmentGoal;
+    String lengthOfTime;
+    String cause;
+
+    public reasonForMassageTable() {
+    }
+
+    public reasonForMassageTable(int customerNumber, String pastTreatment, String treatmentGoal, String lengthOfTime, String cause) {
+        this.customerNumber = customerNumber;
+        this.pastTreatment = pastTreatment;
+        this.treatmentGoal = treatmentGoal;
+        this.lengthOfTime = lengthOfTime;
+        this.cause = cause;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getPastTreatment() {
+        return pastTreatment;
+    }
+
+    public void setPastTreatment(String pastTreatment) {
+        this.pastTreatment = pastTreatment;
+    }
+
+    public String getTreatmentGoal() {
+        return treatmentGoal;
+    }
+
+    public void setTreatmentGoal(String treatmentGoal) {
+        this.treatmentGoal = treatmentGoal;
+    }
+
+    public String getLengthOfTime() {
+        return lengthOfTime;
+    }
+
+    public void setLengthOfTime(String lengthOfTime) {
+        this.lengthOfTime = lengthOfTime;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+}
