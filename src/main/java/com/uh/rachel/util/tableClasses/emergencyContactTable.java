7cf659127f79924b413f8237c35ebbdfffb970ef
rochelljackson1 <rochelljackson1@gmail.com>
2020-11-04 13:20:26 -0600

    Rachel 11/04/2020 @1:21 PM
    Making tableClasses

---
+++ src/main/java/com/uh/rachel/util/tableClasses/emergencyContactTable.java
@@ -0,0 +1,61 @@
+package com.uh.rachel.util.tableClasses;
+
+public class emergencyContactTable {
+    int customerNumber;
+    String emergencyFirstName;
+    String emergencyLastName;
+    String emergencyPhone;
+    String relationship;
+
+    public emergencyContactTable() {
+
+    }
+
+    public emergencyContactTable(int customerNumber, String emergencyFirstName, String emergencyLastName, String emergencyPhone, String relationship) {
+        this.customerNumber = customerNumber;
+        this.emergencyFirstName = emergencyFirstName;
+        this.emergencyLastName = emergencyLastName;
+        this.emergencyPhone = emergencyPhone;
+        this.relationship = relationship;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getEmergencyFirstName() {
+        return emergencyFirstName;
+    }
+
+    public void setEmergencyFirstName(String emergencyFirstName) {
+        this.emergencyFirstName = emergencyFirstName;
+    }
+
+    public String getEmergencyLastName() {
+        return emergencyLastName;
+    }
+
+    public void setEmergencyLastName(String emergencyLastName) {
+        this.emergencyLastName = emergencyLastName;
+    }
+
+    public String getEmergencyPhone() {
+        return emergencyPhone;
+    }
+
+    public void setEmergencyPhone(String emergencyPhone) {
+        this.emergencyPhone = emergencyPhone;
+    }
+
+    public String getRelationship() {
+        return relationship;
+    }
+
+    public void setRelationship(String relationship) {
+        this.relationship = relationship;
+    }
+}
