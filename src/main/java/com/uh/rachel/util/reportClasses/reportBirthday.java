37905195ef8586e8defe59ab3fd6c20242c11d4d
makki1984 <73632646+makki1984@users.noreply.github.com>
2020-11-12 02:25:17 -0600

    Add files via upload

---
+++ src/main/java/com/uh/rachel/util/reportClasses/reportBirthday.java
@@ -0,0 +1,34 @@
+package com.uh.rachel.util.reportClasses;
+import com.uh.rachel.util.tableClasses.Address;
+import com.uh.rachel.util.tableClasses.customerTable;
+
+public class reportBirthday {
+    customerTable customerPackages;
+    Address AddressPackage;
+
+
+    public reportBirthday(customerTable customerPackages, Address AddressPackage) {
+        this.customerPackages = customerPackages;
+        this.AddressPackage = AddressPackage;
+
+    }
+
+
+
+    public customerTable getCustomerPackages() {
+        return customerPackages;
+    }
+
+    public void setCustomerPackages(customerTable customerPackages) {
+        this.customerPackages = customerPackages;
+    }
+
+    public Address getAddressPackage() {
+        return AddressPackage;
+    }
+
+    public void setAddressPackage(Address AddressPackage) {
+        this.AddressPackage = AddressPackage;
+    }
+
+}
