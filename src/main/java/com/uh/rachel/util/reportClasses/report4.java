577705454914d67303e1be49011be83fffd9d6ac
nnchowd2 <nafisachowdhury23@gmail.com>
2020-11-12 23:26:18 -0600

    Nafisa report class

---
+++ src/main/java/com/uh/rachel/util/reportClasses/report4.java
@@ -0,0 +1,30 @@
+package com.uh.rachel.util.reportClasses;
+
+import com.uh.rachel.util.tableClasses.customerTable;
+import com.uh.rachel.util.tableClasses.companyClientHistoryTable;
+
+public class report4 {
+    customerTable customerPackage;
+    companyClientHistoryTable companyClientHistoryPackage;
+
+    public report4(customerTable customerPackage, companyClientHistoryTable companyClientHistoryPackage) {
+        this.customerPackage = customerPackage;
+        this.companyClientHistoryPackage = companyClientHistoryPackage;
+    }
+
+    public customerTable getCustomerPackage() {
+        return customerPackage;
+    }
+
+    public void setCustomerPackage(customerTable customerPackage) {
+        this.customerPackage = customerPackage;
+    }
+
+    public companyClientHistoryTable getCompanyClientHistoryPackage() {
+        return companyClientHistoryPackage;
+    }
+
+    public void setCompanyClientHistoryPackage(companyClientHistoryTable companyClientHistoryPackage) {
+        this.companyClientHistoryPackage = companyClientHistoryPackage;
+    }
+}
