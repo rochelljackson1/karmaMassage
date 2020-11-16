bb5cff6112a19ac1cd14e50dccc4aff238af2c8b
Yeslyn Casasola <72273586+CasasolaYeslyn@users.noreply.github.com>
2020-11-16 16:04:54 -0600

    Update deletePackages.java

--- src/main/java/com/uh/rachel/util/servlets/deletePackages.java
+++ src/main/java/com/uh/rachel/util/servlets/deletePackages.java
@@ -7,20 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-        import com.uh.rachel.util.DataHandler;
-
-        import javax.servlet.ServletException;
-        import javax.servlet.annotation.WebServlet;
-        import javax.servlet.http.HttpServlet;
-        import javax.servlet.http.HttpServletRequest;
-        import javax.servlet.http.HttpServletResponse;
-        import java.io.IOException;
-
 @WebServlet(name = "Options")
 public class deletePackages extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        DataHandler.deleteDoctorRowByID(Integer.parseInt(request.getParameter("rowToDelete")));
+        DataHandler.deletePackagesRowByID(Integer.parseInt(request.getParameter("rowToDelete")));
         response.sendRedirect("./packages.jsp");
     }
 }
